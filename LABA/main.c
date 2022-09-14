59a5e67a6936e36685bb689953f7b5c4a1127290
Ilya Burlak <113059204+IlyaBurlak@users.noreply.github.com>
2022-09-14 11:39:38 +0300

    Laba 1

--- LABA/main.c
+++ LABA/main.c
@@ -7,8 +7,13 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(){
+    char a[200] ;
+    float b ;
+    printf( "Введите строку: ");
+    scanf("%s" , a);             // Ввод строки
+    printf("Введите Вещественное число: ");
+    scanf("%f" , &b);            // Ввод вещественное число
+    printf("Строка:%s \n" "Вещественное число: %f \n" , a , b); //Вывод ТЗ
     return 0;
 }
