f474b90f60816f0f35134fec724eb0b080811bdd
Ilya Burlak <113059204+IlyaBurlak@users.noreply.github.com>
2022-09-14 11:35:39 +0300

    Initial Commit

---
+++ LABA/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LABA
+//
+//  Created by Илья Бурлак on 14.09.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
