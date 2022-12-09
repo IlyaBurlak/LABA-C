23637309aaa99b249bde930b93e04e04eaebdc55
Ilya Burlak <113059204+IlyaBurlak@users.noreply.github.com>
2022-12-09 23:14:29 +0300

    11 lab.Heder
    1

---
+++ 11 laba.h
@@ -0,0 +1,56 @@
+
+//
+//  Lab_11.h
+//  learning_C
+//
+//  Created by Илья Бурлак on 24.11.2022.
+//
+
+#ifndef Lab_11_h
+#define Lab_11_h
+
+#include <stdio.h>
+
+struct Circle {
+    float pi;
+    float radius;
+    float diametr;
+    float square;
+    float circle_length;
+};
+
+float square(struct Circle al){
+    
+    return al.radius *al.radius *al.pi;
+   
+    }
+
+float circles(struct Circle al){
+    return 2*al.pi*al.radius;
+}
+
+void NewStruct(){
+    struct Circle al;
+    float pi , r;
+    printf("Введите число pi ");
+    scanf("%f" ,  &pi);
+    printf("Введите радиус ");
+    scanf("%f/n" ,  &r);
+    al.pi = pi;
+    al.radius = r;
+    al.diametr = al.radius*2;
+    al.circle_length = circles(al);
+    al.square = square(al);
+    
+}
+
+void printStruct(){
+    struct Circle a;
+    printf("Радиус : %f\n" , a.radius);
+    printf("Диаметр : %f\n" , a.diametr);
+    printf("Длина дуги : %f\n" , a.circle_length);
+    printf("Площадь : %f\n" , a.square);
+    
+}
+
+#endif //  Lab_11_h
