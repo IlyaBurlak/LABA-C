3151341093e5881e92fcb605d48fae2f7852b188
Ilya Burlak <113059204+IlyaBurlak@users.noreply.github.com>
2022-12-09 23:15:05 +0300

    Create 11 lab.c

---
+++ 11 lab.c
@@ -0,0 +1,16 @@
+//
+//  Lab_11.c
+//  learning_C
+//
+//  Created by Илья Бурлак on 24.11.2022.
+//
+
+#include "Lab_11.h"
+#include <stdio.h>
+
+int main(){
+    NewStruct();
+    printStruct();
+    
+    return 0;
+}
