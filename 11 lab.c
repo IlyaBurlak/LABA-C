218cc57b8bb79054b846f8f0843a531e533badba
Ilya Burlak <113059204+IlyaBurlak@users.noreply.github.com>
2022-12-21 22:33:45 +0300

    Update 11 lab.c

--- 11 lab.c
+++ 11 lab.c
@@ -8,6 +8,47 @@
 #include "Lab_11.h"
 #include <stdio.h>
 
+struct Circle {
+    float pi;
+    float radius;
+    float diametr;
+    float square;
+    float circle_length;
+};
+
+float square(struct Circle al){
+    
+    return al.radius *al.radius *al.pi;
+   
+    }
+
+float circles(struct Circle al){
+    return 2*al.pi*al.radius;
+}
+
+void NewStruct(){
+    struct Circle al;
+    float pi , r;
+    printf("Введите число pi ");
+    scanf("%f" ,  &pi);
+    printf("Введите радиус ");
+    scanf("%f/n" ,  &r);
+    al.pi = pi;
+    al.radius = r;
+    al.diametr = al.radius*2;
+    al.circle_length = circles(al);
+    al.square = square(al);
+    
+}
+
+void printStruct(){
+    struct Circle a;
+    printf("Радиус : %f\n" , a.radius);
+    printf("Диаметр : %f\n" , a.diametr);
+    printf("Длина дуги : %f\n" , a.circle_length);
+    printf("Площадь : %f\n" , a.square);
+    
+}
 int main(){
     NewStruct();
     printStruct();
